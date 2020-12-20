d6ebc65b29634aa1784efc2c468bcb3e7592e980
hiroshi oshiro <ooshiro010@gmail.com>
2020-12-20 15:44:45 +0900

    [update]Order detail UI (p72)

--- TapiokaMilkTea/TapiokaMilkTea/Scene/OrderDetailView.swift
+++ TapiokaMilkTea/TapiokaMilkTea/Scene/OrderDetailView.swift
@@ -9,20 +9,35 @@ import SwiftUI
 
 struct OrderDetailView: View {
     var order: OrderEntity
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        return formatter
+    }
     
     var body: some View {
         VStack {
             Text(order.flavorName)
                 .font(.title)
             if order.nataDeCoco {
-                Text("nata de coco")
+                Text("Nata de coco")
             }
+            if order.iceCream != 0 {
+                Text(order.iceCreamName)
+            }
+            Text(order.other)
+            Text("\(order.date, formatter: dateFormatter)")
+                .font(.caption)
         }
     }
 }
 
 struct OrderDetail_Previews: PreviewProvider {
     static var previews: some View {
-        OrderDetailView(order: orderStore.orders[0])
+        Group{
+            OrderDetailView(order: orderStore.orders[0])
+            OrderDetailView(order: orderStore.orders[1])
+        }
     }
 }
